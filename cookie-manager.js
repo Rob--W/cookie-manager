d435076c3cb990a6e85451baa8af427f99c45e91
Rob Wu <rob@robwu.nl>
2017-03-29 15:05:33 +0200

    Add SameSite tag if applicable

--- cookie-manager.js
+++ cookie-manager.js
@@ -338,6 +338,8 @@ function renderCookie(cookiesOut, cookie) {
     if (cookie.httpOnly) extraInfo.push('httpOnly');
     if (cookie.storeId === '1') extraInfo.push('incognito');
     else if (cookie.storeId === 'firefox-private') extraInfo.push('private');
+    if (cookie.sameSite === 'lax') extraInfo.push('SameSite=lax');
+    else if (cookie.sameSite === 'strict') extraInfo.push('SameSite=strict');
     extraInfo = extraInfo.join(', ');
     row.insertCell(5).textContent = extraInfo;
 
