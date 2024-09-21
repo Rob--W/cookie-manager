Rob--W/cookie-manager

commit 1c4505afdd1540ef81ceeac685f6091f4739cb4b

Fix importing cookies with unspecified sameSite

cookie-manager.js
@@ -1259,7 +1259,7 @@ document.getElementById('importform').onsubmit = function(event) {
         document.getElementById('import-cancel').disabled = true;
 
         var deleteSameSite = cookies.some(c => 'sameSite' in c) && !chrome.cookies.SameSiteStatus;
-        var convertSameSiteUnspecified = !deleteSameSite && chrome.cookies.SameSiteStatus.UNSPECIFIED;
+        var convertSameSiteUnspecified = !deleteSameSite && !chrome.cookies.SameSiteStatus.UNSPECIFIED;
         var deleteFirstPartyDomain = cookies.some(c => 'firstPartyDomain' in c) && !checkFirstPartyDomainSupport();
         var deletePartitionKey = cookies.some(c => 'partitionKey' in c) && !checkPartitionKeySupport();
 

cookie-manager.html
@@ -163,7 +163,7 @@ By <a href="https://robwu.nl">Rob Wu</a>.
         <legend>Same-site status</legend>
         <select id="editform.sameSite">
             <option value="unspecified" selected>Unspecified (default)</option>
-            <option value="no_restriction" selected>No restriction</option>
+            <option value="no_restriction">No restriction</option>
             <option value="lax">Lax</option>
             <option value="strict">Strict</option>
         </select>

commit 2e9691a34e4f9d8dc6b639a210e1479e2d4ff7c4

Fix save button in Firefox when SameSite unspecified is selected (#62)
Regression caused by 1c4505afdd1540ef81ceeac685f6091f4739cb4b (#54)

cookie-manager.js
@@ -652,6 +652,18 @@ document.getElementById('editform').onsubmit = function(event) {
     cookie.httpOnly = document.getElementById('editform.httpOnly').checked;
     if (!document.getElementById('editform.sameSiteBox').hidden) {
         cookie.sameSite = document.getElementById('editform.sameSite').value;
+        if (
+            cookie.sameSite === 'unspecified' &&
+            // Note: chrome.cookies.SameSiteStatus is non-void because
+            // otherwise the sameSiteBox would have been hidden.
+            !chrome.cookies.SameSiteStatus.UNSPECIFIED &&
+            chrome.cookies.SameSiteStatus.NO_RESTRICTION
+        ) {
+            // Firefox doesn't support SameSite=unspecified in the cookies API.
+            // https://bugzilla.mozilla.org/show_bug.cgi?id=1550032
+            // It defaults to no_restriction.
+            cookie.sameSite = "no_restriction";
+        }
     }
     if (document.getElementById('editform.sessionFalse').checked) {
         cookie.expirationDate = dateToExpiryCompatibleTimestamp(document.getElementById('editform.expiry'));